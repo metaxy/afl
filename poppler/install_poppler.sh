6d6834d42f7b1209b55b21da6d8a1943c81134ac
Paul Walger <metaxy@walger.name>
2014-12-19 13:51:01 +0100

    poppler

--- poppler/install_poppler.sh
+++ poppler/install_poppler.sh
@@ -1,6 +1,8 @@
 wget http://poppler.freedesktop.org/poppler-0.29.0.tar.xz
 tar -Jxf poppler-0.29.0.tar.xz
 cd  poppler-0.29.0
+rm poppler-0.29.0.tar.xz
 mkdir build
+cd build
 cmake -DCMAKE_CXX_COMPILER=afl-g++ ..
 make
