c2df41d231e4b5b18ade12d2dc1fd99bd362bc7d
Paul Walger <metaxy@walger.name>
2014-12-20 13:42:59 +0100

    crash poppler on 64bit in liblcms

--- poppler/install_poppler.sh
+++ poppler/install_poppler.sh
@@ -1,7 +1,7 @@
 wget http://poppler.freedesktop.org/poppler-0.29.0.tar.xz
 tar -Jxf poppler-0.29.0.tar.xz
+rm poppler-0.29.0.tar.xz -f
 cd  poppler-0.29.0
-rm poppler-0.29.0.tar.xz
 mkdir build
 cd build
 cmake -DCMAKE_CXX_COMPILER=afl-g++ ..
